f351621c899bce5d042c45451ebe8c61cc6c7539
MiSiEkHASH <93140052+MiSiEkHASH@users.noreply.github.com>
2022-09-23 15:55:24 +0200

    some change

--- auto_update_daemon.bat
+++ auto_update_daemon.bat
@@ -1,11 +1,15 @@
 @echo off
 echo test sw - auto update roc_api
+echo %date%:%time:~0,5%: Start application.>>logfile.log
 echo.
+timeout 5 > nul
 
 :loop
 set timer=%TIME:~0,5%
-if "%timer%"=="14:08" (
-	echo ok, now is update time!
+set /p checkUpgradeTime=<upgrade_time.dat
+if "%timer%"=="%checkUpgradeTime%" (
+	echo Ok, now is update time!
+	echo %date%:%time:~0,5%: Ok, now is update time!>>logfile.log
 	timeout 1 > nul
 	goto upgrade
 ) else (
@@ -17,12 +21,14 @@ goto loop
 
 :upgrade
 cls
-echo so, looking for ROCPM API upgrade...
+echo Looking for ROCPM API upgrade...
+echo %date%:%time:~0,5%: Looking for ROCPM API upgrade...>>logfile.log
 set /p currentSW=<C:\Aplitt\Aplitt.ROC.PaymentMachine.API\api_ver.ini
 for /F "delims=" %%A in ('curl -s -L https://raw.githubusercontent.com/MiSiEkHASH/ROC_API_UPDATE/main/api_ver.dat') do set lastSW=%%A
 if "%lastSW%"=="%currentSW%" (
 	echo.
 	echo Lastest ROC_API software is installed, no need to upgrade!
+	echo %date%:%time:~0,5%: Lastest %lastSW% ROC_API software is installed, no need to upgrade!>>logfile.log
 	echo.
 	timeout 62
 	cls
@@ -30,7 +36,8 @@ if "%lastSW%"=="%currentSW%" (
 ) else (
 	echo.
 	echo Found new ROC_API software: %lastSW%, checking approval to update...
-	timeout 2 > nul
+	echo %date%:%time:~0,5%: Found new ROC_API software: %lastSW%, checking approval to update...>>logfile.log
+	timeout 3 > nul
 	echo.
 	cls
 	goto CheckUpgradeAproval
@@ -41,12 +48,14 @@ for /F "delims=" %%A in ('curl -s -L https://raw.githubusercontent.com/MiSiEkHAS
 if "%aprovalUpgrade%"=="allowed" (
 	echo.
 	echo Upgrade ROC_API software: %lastSW% allowed! Installing...
-	timeout 2 > nul
+	echo %date%:%time:~0,5%: Upgrade ROC_API software: %lastSW% allowed! Installing...>>logfile.log
+	timeout 3 > nul
 	echo.
 ) else (
 	echo.
 	echo Upgrade ROC_API software not allowed! Skipping installation...
-	timeout 20 > nul
+	echo %date%:%time:~0,5%: Upgrade ROC_API software not allowed! Skipping installation...>>logfile.log
+	timeout 62 > nul
 	echo.
 	cls
 	goto loop
